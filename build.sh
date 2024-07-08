dad378fd6733c0b0d8a85d8439bed695b0ed6e19
RasmusHogslatt <r.hogslatt@gmail.com>
2024-07-08 20:00:46 +0200

    ss

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ echo "Building frontend..."
 cd frontend || exit
 echo "Contents of frontend directory:"
 ls -la
+
+echo "Renaming cargo.toml to Cargo.toml if necessary"
+[ -f cargo.toml ] && mv cargo.toml Cargo.toml
+
+echo "Contents of frontend directory after renaming:"
+ls -la
+
 trunk build --release
 cd ..
 
@@ -18,6 +25,10 @@ echo "Building backend..."
 cd backend || exit
 echo "Contents of backend directory:"
 ls -la
+echo "Contents of backend/src directory:"
+ls -la src
+echo "Contents of backend Cargo.toml:"
+cat Cargo.toml
 cargo build --release
 cd ..
 
