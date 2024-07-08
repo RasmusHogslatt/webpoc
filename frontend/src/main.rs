17a7673839f2e1faa35c15c13566be503fb1ef24
RasmusHogslatt <r.hogslatt@gmail.com>
2024-07-09 00:37:26 +0200

    ok

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(frontend::TemplateApp::new(cc))),
+        Box::new(|cc| Ok(Box::new(frontend::TemplateApp::new(cc)))),
     )
 }
 
@@ -34,7 +34,7 @@ fn main() {
             .start(
                 "the_canvas_id",
                 web_options,
-                Box::new(|cc| Box::new(frontend::TemplateApp::new(cc))),
+                Box::new(|cc| Ok(Box::new(frontend::TemplateApp::new(cc)))),
             )
             .await;
 
