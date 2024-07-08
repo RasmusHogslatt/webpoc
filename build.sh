77c6b2d4a6f3304afa6a251216db5467447d8c09
RasmusHogslatt <r.hogslatt@gmail.com>
2024-07-08 13:34:09 +0200

    wasi test

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 set -e
 
 # Build the WebAssembly binary
-cargo build --release --target wasm32-unknown-unknown
+# cargo build --release --target wasm32-unknown-unknown
+cargo build --release --target wasm32-wasi
 
 # Use wasm-bindgen to generate JavaScript bindings
-wasm-bindgen --out-dir ./dist --target web target/wasm32-unknown-unknown/release/your_app_name.wasm
+# wasm-bindgen --out-dir ./dist --target web target/wasm32-unknown-unknown/release/your_app_name.wasm
+wasm-bindgen --out-dir ./dist --target web target/wasm32-wasi/release/your_app_name.wasm
 
 # Copy your index.html and any other static assets to the dist folder
 # cp index.html dist/
