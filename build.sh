42a73b6e2d47eab774555cda4b3946f33b652292
RasmusHogslatt <r.hogslatt@gmail.com>
2024-07-09 20:04:42 +0200

    s

--- build.sh
+++ build.sh
@@ -7,24 +7,12 @@ pwd
 echo "Contents of current directory:"
 ls -la
 
-# echo "Building shared.."
-# echo "Contents of shared directory:"
-# ls -la
-# cargo build --release --package shared
-
 echo "Building frontend..."
 cd frontend || exit
-echo "Contents of frontend directory:"
-ls -la
-
 trunk build --release
 cd ..
 
 echo "Building backend..."
-echo "Contents of backend directory:"
-cd backend || exit
-ls -la
-cd ..
 
 # Run cargo build from the root for backend
 cargo build --release --package backend
