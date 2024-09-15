fd16af89a4591b18e27b3fd51882e0b91d1e3618
RasmusHogslatt <r.hogslatt@gmail.com>
2024-09-15 13:00:11 +0200

    s

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,10 +5,23 @@ use crate::api::auth;
 use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use db::*;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     init_db().expect("Failed to initialize database");
+
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file(
+            "/etc/letsencrypt/live/rasmushogslatt.com/privkey.pem",
+            SslFiletype::PEM,
+        )
+        .unwrap();
+
+    builder
+        .set_certificate_chain_file("/etc/letsencrypt/live/rasmushogslatt.com/fullchain.pem")
+        .unwrap();
 
     HttpServer::new(|| {
         let cors = Cors::default()
@@ -22,7 +35,7 @@ async fn main() -> std::io::Result<()> {
             .service(auth::register_user)
             .service(auth::update_user_data)
     })
-    .bind("[::]:8080")?
+    .bind_openssl("[::]:443", builder)?
     .run()
     .await
 }
